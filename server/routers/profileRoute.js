3b6b1171287e9717864c4e44d35adbc371426483
trandat1803 <quocdat@MacprocuaDatne.local>
2023-03-12 19:40:19 +0700

    Dat|update|delete unuse file

--- server/routers/profileRoute.js
+++
@@ -1,6 +0,0 @@
-const auth = require('../middleware/auth');
-const express = require('express');
-const router = express.Router();
-const profileHandler = require('../controller/profileHandler');
-
-router.get('/edit', auth, profileHandler.editProfile);
